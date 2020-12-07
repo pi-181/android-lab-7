377df636ab080321a363f92294421aebdc6cd3ed
demkom58 <demkom58@gmail.com>
2020-12-07 07:24:53 +0200

    Initial commit

---
+++ app/src/main/java/com/demkom58/androidlab7/MainActivity.java
@@ -0,0 +1,64 @@
+package com.demkom58.androidlab7;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView tvOut;
+    private TextView tvLon;
+    private TextView tvLat;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvOut = findViewById(R.id.textView1);
+        tvLon = findViewById(R.id.longitude);
+        tvLat = findViewById(R.id.latitude);
+
+        // Получаем сервис
+        LocationManager locManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        LocationListener locListener = new LocationListener() {
+            public void onLocationChanged(Location location) {
+                // Called when a new location is found by the network location provider.
+                tvLat.append("\n" + location.getLatitude());
+                tvLon.append("\n" + location.getLongitude());
+            }
+
+            public void onStatusChanged(String provider, int status, Bundle extras) { }
+
+            public void onProviderEnabled(String provider) { }
+
+            public void onProviderDisabled(String provider) { }
+
+        };
+
+        //Підписуємося на зміни у свідченнях датчика
+        if (hasPermission(Manifest.permission.ACCESS_FINE_LOCATION)) {
+            Toast.makeText(this, "Requesting updates", Toast.LENGTH_SHORT);
+            locManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locListener);
+        } else {
+            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        }
+
+        // Якщо gps включений, то ..., інакше вивести "GPS is not turned on.".
+        if (locManager.isProviderEnabled(LocationManager.GPS_PROVIDER))
+            tvOut.setText("GPS is turned on...");
+        else
+            tvOut.setText("GPS is not turned on...");
+    }
+
+    private boolean hasPermission(String perm) {
+        return(PackageManager.PERMISSION_GRANTED == checkSelfPermission(perm));
+    }
+
+}
